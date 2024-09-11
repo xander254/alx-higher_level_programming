cc102beda790e102b144af858be4789173a1d0b7
xander254 <alexkinyanjui356@gmail.com>
2024-09-11 21:51:09 +0300

    a sript that checks the last digit and using it to make a decision

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 print(f"Last digit of {number} ", end="")
-last_digit = abs(number) % 10
+if number < 0:
+    last_digit = (abs(number) % 10)
+else:
+    last_digit = number % 10
 print(f"is {last_digit} ", end="")
 if last_digit > 5:
     print("and is greater than 5")
