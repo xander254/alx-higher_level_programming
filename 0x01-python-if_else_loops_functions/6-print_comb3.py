702eff80f9252aabcd8dcb66d1d129cc9f3a0534
xander254 <alexkinyanjui356@gmail.com>
2024-09-12 14:56:40 +0300

    Fixed: I had to use the format string. Print a combination of numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for digit1 in range(0,9):
     for digit2 in range((digit1 + 1), 10):
         if (digit1 == 0 and digit2 == 9):
-            print(f"{digit1}{digit2}", end="")
+            print("{}{}".format(digit1,digit2), end="")
         else:
-            print(f"{digit1}{digit2}", end=", ")
+            print("{}{}".format(digit1,digit2), end=", ")
 print()
