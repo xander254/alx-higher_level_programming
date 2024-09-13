4863156f3b4f2d683e100f37f951853c12a5ab75
xander254 <alexkinyanjui356@gmail.com>
2024-09-13 03:36:09 +0300

    Fixed: 0 arguments bug>> A program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,11 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv
     n = len(argv)
+    if n == 1:
+        print("0 arguments.")
     if n == 2:
         print("1 argument:")
-    else:
+    elif n > 2:
         print("{} arguments:".format(n - 1))
     for args in range(1, n):
         print("{}: {}".format(args, argv[args]))
