1cb1a8691a9e6a523e575b40d25a6c159098af93
xander254 <alexkinyanjui356@gmail.com>
2024-09-15 16:22:27 +0300

    Print items in a list where every item should be in its own line

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# Define the function
 def print_list_integer(my_list=[]):
-    for lista in my_list:
-        print("{}".format(lista))
+#loop through the list using the length of the list
+    for index in range(len(my_list)):
+#print items at a specific index
+        print("{}".format(my_list[index]))
