766b5827d9ea46b3cb16a72d073a8d1853eea385
xander254 <alexkinyanjui356@gmail.com>
2024-09-15 16:24:08 +0300

    Fix: Pycodestyle issues.   Print items in a list where every item should be in its own line

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Define the function
 def print_list_integer(my_list=[]):
-#loop through the list using the length of the list
+    # loop through the list using the length of the list
     for index in range(len(my_list)):
-#print items at a specific index
+        # print items at a specific index
         print("{}".format(my_list[index]))
