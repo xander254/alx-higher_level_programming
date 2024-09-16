3277856dbdefc07c92c27f24623c3dfc0f6f4017
xander254 <alexkinyanjui356@gmail.com>
2024-09-17 00:13:54 +0300

    a function that adds 2 tuples.wq

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# define the function
+def add_tuple(tuple_a=(), tuple_b=()):
+    # access the elements of a tuple using a loop,add them,store
+    # them in another tupple
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
+    elif len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+    first_sum = tuple_a[0] + tuple_b[0]
+    second_sum = tuple_a[1] + tuple_b[1]
+    new_tuple = (first_sum, second_sum)
+    return new_tuple
