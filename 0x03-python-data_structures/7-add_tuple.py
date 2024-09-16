b186ea68b09fbc6e6dbff133dfb71024b4072ef9
xander254 <alexkinyanjui356@gmail.com>
2024-09-17 02:49:35 +0300

    add tupple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,8 +5,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # them in another tupple.
     if len(tuple_a) < 2:
         tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
-    elif len(tuple_b) < 2:
+    else:
+        tuple_a = tuple_a[:2]
+    if len(tuple_b) < 2:
         tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+    else:
+        tuple_b = tuple_b[:2]
     first_sum = tuple_a[0] + tuple_b[0]
     second_sum = tuple_a[1] + tuple_b[1]
     new_tuple = (first_sum, second_sum)
