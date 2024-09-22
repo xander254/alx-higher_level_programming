5a8345da1cbde4f035e265c6f1bc6579991d4b71
xander254 <alexkinyanjui356@gmail.com>
2024-09-23 01:03:37 +0300

    Bug fix: used symetric differencce instead:) a function that returns a set of all elements present in only one set.

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 # def func
 def only_diff_elements(set_1, set_2):
-    diff = (set_1.difference(set_2))
-    diff2 = (set_2.difference(set_2))
-    diffval1 = list(diff)
-    diffval2 = list(diff2)
-    result = diffval1 + diffval2
+    result = set_1 ^ set_2
     return result
