6b05419daf81321fab077c6f779cc03b7cda3504
xander254 <alexkinyanjui356@gmail.com>
2024-10-08 15:26:33 +0300

    Fix: Typo and doc style documentation and perimeter logic.....find the area and perimeter of a class Rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
 
     @property
     def height(self):
-        return height.__self
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -43,9 +43,16 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """
+        Returna area of rectangle
+        """
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width or self.__height == 0:
-            perimeter = 0
+        """
+        Returns perimeter of rectangle
+        if either height/ width == 0 perimeter is 0
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
