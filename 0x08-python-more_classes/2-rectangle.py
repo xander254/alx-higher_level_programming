e255ff7fe9e7a37f4259992fca8f3017b550f930
xander254 <alexkinyanjui356@gmail.com>
2024-10-08 15:20:19 +0300

    find the area and perimeter of a class Rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+A module that create a class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class that represents a rectangle
+    This class allows setting of height and width
+    """
+    def __init__(self, width=0, height=0):
+        """
+        initialization of a class Rectangle
+        :param: width (int) the width of the rectangle
+        :param: height (int) the height of the reactangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return height.__self
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width or self.__height == 0:
+            perimeter = 0
+        return 2 * (self.__width + self.__height)
