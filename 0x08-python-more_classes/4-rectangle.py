beb6e083e514001065e439b6facde770813b2409
xander254 <alexkinyanjui356@gmail.com>
2024-10-09 17:18:56 +0300

    use repr to execute a new instance using eval

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+A module that create a class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class that represents a rectangle
+    This class allows setting of height and width
+    """
+    def __init__(self, width=0, height=0):
+        """
+        initialization of a class Rectangle
+        :param width: (int) the width of the rectangle
+        :param height: (int) the height of the reactangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Returna area of rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Returns perimeter of rectangle
+        if either height/ width == 0 perimeter is 0
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """
+        returns a string representation of a rectngle using #
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join("#" * self.__width for _ in range(self.__height))
+
+    def __repr__(self):
+        """
+        returns the repr representation of rectangle
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
