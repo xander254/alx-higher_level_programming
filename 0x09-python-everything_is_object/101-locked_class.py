090d4efde69b59293aba356fda27caafd9d44169
xander254 <alexkinyanjui356@gmail.com>
2024-10-10 04:56:47 +0300

    This class is locked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,3 +6,6 @@ class LockedClass:
     A locked class that cant allow creation of attributes
     """
     __slots__ = ['first_name']
+
+    def __init__(self, first_name=None):
+        self.first_name = first_name
