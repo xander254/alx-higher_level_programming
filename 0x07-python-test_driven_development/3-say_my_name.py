a1ff8a1a9d5f89dbad1be7f793351a91652cdb5d
xander254 <alexkinyanjui356@gmail.com>
2024-10-11 21:27:53 +0300

    Fix: pycodestyle error...a function that prints My name is <first name> <last name>

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,7 +15,7 @@ def say_my_name(first_name, last_name=""):
     Returns:
         A string My name is <first name> <last name>
     """
-    
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
