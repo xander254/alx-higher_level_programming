a995f4b917c7798918c6f7900b12841e0db5e36d
xander254 <alexkinyanjui356@gmail.com>
2024-10-11 21:25:24 +0300

    a function that prints My name is <first name> <last name>

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A module that prints "My name is <first name> <last name>"
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    a function that prints My name is <first name> <last name>
+
+    Args:
+        first_name (string): the string that is the first name
+        last_name (string): the string that is the last name
+
+    Returns:
+        A string My name is <first name> <last name>
+    """
+    
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
