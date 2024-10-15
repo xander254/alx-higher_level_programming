5384f28b584329c82248a4558a841224ebfb9b53
xander254 <alexkinyanjui356@gmail.com>
2024-10-16 01:06:20 +0300

    Fixed reading.....a function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,5 +11,5 @@ def read_file(filename=""):
         filename: the file name to be read
     Return: The data
     """
-    with open('my_file_0.txt', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
