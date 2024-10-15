666280c0aab6b101facd5a9500ea8a905c186bdb
xander254 <alexkinyanjui356@gmail.com>
2024-10-15 23:03:28 +0300

    a function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A module that reads a text file and prints to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    A function that reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: the file name to be read
+    Return: The data
+    """
+    with open('my_file_0.txt', 'r', encoding="utf-8") as f:
+        read_text = f.read()
+    print("{}".format(read_text), end="")
