9d15ced16a8188fec3f08b98d44d98114d40cb93
xander254 <alexkinyanjui356@gmail.com>
2024-10-18 22:15:49 +0300

    Fix: retrun a new dict....a function that returns a new dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,4 +2,5 @@
 def multiply_by_2(a_dictionary):
     for key, value in a_dictionary.items():
         a_dictionary[key] = value * 2
-    return a_dictionary
+    dict2 = a_dictionary
+    return dict2
