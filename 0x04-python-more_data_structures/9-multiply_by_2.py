1168fffba899bdf2bcb3571326fa689885b57664
xander254 <alexkinyanjui356@gmail.com>
2024-10-18 22:22:50 +0300

    Fix: retrun a new dictionary after func runs....a function that returns a new dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    dict2 = {}
     for key, value in a_dictionary.items():
-        a_dictionary[key] = value * 2
-    dict2 = a_dictionary
+        dict2[key] = value * 2
     return dict2
