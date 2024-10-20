3173cefd60e4cc94b40904954cbb50aaca4c0f7f
xander254 <alexkinyanjui356@gmail.com>
2024-10-20 16:26:11 +0300

    Fix: Google style documentation...from json to string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""
+A module that uses json to convert a json string to an object
+"""
 import json
 
 
 def from_json_string(my_str):
     """
     A function that retruns a py obj represented by a json string
+    Args:
+        my_str: the json string that will be used 
     """
     loaded = json.loads(my_str)
     return loaded
