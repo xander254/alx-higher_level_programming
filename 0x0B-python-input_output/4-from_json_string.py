458bced32f7ca430afce5d2763185ff91b385712
xander254 <alexkinyanjui356@gmail.com>
2024-10-20 16:20:20 +0300

    from json to string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -6,5 +6,5 @@ def from_json_string(my_str):
     """
     A function that retruns a py obj represented by a json string
     """
-    dump = json.loads(my_str)
-    return dump
+    loaded = json.loads(my_str)
+    return loaded
