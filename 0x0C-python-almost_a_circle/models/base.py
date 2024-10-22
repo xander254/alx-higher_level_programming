db816b4889bd0b8dbe9347c63fec5b1ddd99f464
xander254 <alexkinyanjui356@gmail.com>
2024-10-22 13:56:11 +0300

    Initial class test commit...no unit tests added

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A module that creates the first class
+"""
+
+
+class Base:
+    """
+    The base class Base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize the class wit an id attribute
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    def __str__(self):
+        return f"{self.id}"
