8bb64c518663fe59c07115bc69ed9535fecae8d3
xander254 <alexkinyanjui356@gmail.com>
2024-10-26 17:06:10 +0300

    Overide display from the rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,6 +30,42 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """
+        A method that updates the dimensions based on orgs & kwargs
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "height" in kwargs:
+                self.height = kwargs["height"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["x"]
+
+    def display(self):
+        """
+        A public method to display the rectangle using #
+        """
+        for _ in range(self.y):
+            print(" ")
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width, end="\n")
+
     def __str__(self):
         """
         A dunder method that prints the state of the square
