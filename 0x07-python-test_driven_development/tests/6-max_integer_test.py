09e0c86e3ccc108ad1db233d39f174981ceccb57
xander254 <alexkinyanjui356@gmail.com>
2024-11-09 20:59:42 +0300

    Max integer - Unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,5 +11,20 @@ class TestMaxInt(unittest.TestCase):
     def test_with_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
-    def test_with_values(self):
+    def test_max_at_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    
+    def test_one_element(self):
+        self.assertEqual(max_integer([12]), 12)
+
+    def test_with_negative(self):
+        self.assertEqual(max_integer([-10, -6, -4, -11]), -4)
+
+    def test_with_one_negative(self):
+        self.assertEqual(max_integer([-10]), -10)
+
+    def test_max_in_beg(self):
+        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
+
+    def test_max_at_middle(self):
+        self.assertEqual(max_integer([1, 6, 4]), 6)
