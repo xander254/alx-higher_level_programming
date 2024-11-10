22e96fb9c2aa966d00a8b6f80688f12d9a7d38b0
xander254 <alexkinyanjui356@gmail.com>
2024-11-10 17:22:56 +0300

    Fix: appending of boolean values intead of strings........a function that finds all multiples of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -9,8 +9,8 @@ def divisible_by_2(my_list=[]):
     i = 0
     while i < len(my_list):
         if (my_list[i] % 2) == 0:
-            new_list.append("True")
+            new_list.append(True)
         else:
-            new_list.append("False")
+            new_list.append(False)
         i += 1
     return new_list
