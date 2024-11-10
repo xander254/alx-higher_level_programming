ed4486a6050f7f54e29e0ab5ddbbd79b8bd7fd98
xander254 <alexkinyanjui356@gmail.com>
2024-11-10 16:39:34 +0300

    remove a character at a position in a string

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# A function that creates a cp of a string removing a character at
+# n position
+def remove_char_at(str, n):
+    """
+    A fuinction that removes a character at position n
+    """
+    if n < 0:
+        return str
+    length = len(str)
+    string1 = str[0:n]
+    string2 = str[n + 1:length]
+    return string1 + string2
