73be6f9d162930799697c6d6b8b0b90320b2f3e6
xander254 <alexkinyanjui356@gmail.com>
2024-11-20 19:30:02 +0300

    a script that lists all records of the table second_table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, name IF name EXISTS
+SELECT score, name
 FROM second_table
-ORDER BY DESC;
+WHERE name IS NOT NULL AND name != ""
+ORDER BY score DESC;
