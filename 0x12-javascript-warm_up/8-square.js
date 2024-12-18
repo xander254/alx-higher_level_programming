86900b5d772d69e93ee7366baab665d56353fe6b
xander254 <alexkinyanjui356@gmail.com>
2024-12-19 01:41:12 +0300

    Fix...edge cases.. create a square using the argument provided

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const size = Number(process.argv[2]);
 if (isNaN(size) || size === undefined) {
-  console.log('');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
