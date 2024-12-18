3cda47b1be481a4b8aabc1131c9ff5bb859f680f
xander254 <alexkinyanjui356@gmail.com>
2024-12-19 02:16:50 +0300

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const number = Number(process.argv[2]);
+function factorize (num) {
+  let result = number;
+  if (num === 0 || num === 1) { return 1; }
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
+}
+console.log(factorize(number));
