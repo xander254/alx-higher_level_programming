a8a85bb430a66979c26c8e4bf0dfaf5257cccf5c
xander254 <alexkinyanjui356@gmail.com>
2024-12-19 02:33:26 +0300

    a script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const number = Number(process.argv[2]);
+if (isNaN(number)) {
+  console.log('1');
+}
 function factorize (num) {
   let result = number;
   if (num === 0 || num === 1) { return 1; }
