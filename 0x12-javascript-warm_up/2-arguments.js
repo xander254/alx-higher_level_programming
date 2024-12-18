1d13564b28e1ee063e61715c4fbabcd2be573426
xander254 <alexkinyanjui356@gmail.com>
2024-12-18 22:58:22 +0300

    refactor,,,Check the number of arguments passed to a program and display something

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
   console.log('No argument');
-} else if (process.argv.length < 4) {
+} else if (process.argv.length === 3) {
   console.log('Argument Found');
-} else if (process.argv.length >= 4) {
+} else {
   console.log('Arguments Found');
 }
