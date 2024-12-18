a34034c1e8a98be60a2d9b10369c0cf873c50f36
xander254 <alexkinyanjui356@gmail.com>
2024-12-18 22:56:58 +0300

    Fix...semistandard..Check the number of arguments passed to a program and display something

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length < 3)
-{
-  console.log("No argument");
-}
-else if (process.argv.length < 4)
-{
-  console.log("Argument Found");
-}
-else if (process.argv.length >= 4){
-  console.log("Arguments Found")
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length < 4) {
+  console.log('Argument Found');
+} else if (process.argv.length >= 4) {
+  console.log('Arguments Found');
 }
