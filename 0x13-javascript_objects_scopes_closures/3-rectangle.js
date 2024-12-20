e835e5acd3b4b405e429e7a264d78cec47caeceb
xander254 <alexkinyanjui356@gmail.com>
2024-12-20 11:45:08 +0300

    refactor public method to print rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
     }
   }
 
