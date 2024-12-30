9d7d3db1b98b529790a9ecd489f21ee8211f9bcd
xander254 <alexkinyanjui356@gmail.com>
2024-12-30 23:52:10 +0300

    filter by state name...Fix use format to create the SQL query with the user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
-    word = sys.argv[4]
+    usr_input = sys.argv[4]
 
     db = MySQLdb.connect(host="localhost",
                          port=3306,
@@ -17,8 +17,13 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
-    cur.execute(query, ('%' + word + '%',))
+    query = (
+        "SELECT * FROM states "
+        "WHERE name LIKE '%{}%' "
+        "ORDER BY id ASC"
+    ).format(usr_input)
+
+    cur.execute(query)
 
     rows = cur.fetchall()
     for row in rows:
