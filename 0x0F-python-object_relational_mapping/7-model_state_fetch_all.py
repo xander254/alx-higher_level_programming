33212a8f795cbc57ca285fa4619ddafcbc263f84
xander254 <alexkinyanjui356@gmail.com>
2024-12-31 03:48:18 +0300

    execution before import

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
             .format(sys.argv[1], sys.argv[2], sys.argv[3]),
             pool_pre_ping=True)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-states = session.query(State).order_by(State.id).all()
-for state in states:
-    print("{}: {}".format(state.id, state.name))
-session.close()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
