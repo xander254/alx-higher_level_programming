6dadef9cec58124c55a46aa2b592fd3c3e891634
xander254 <alexkinyanjui356@gmail.com>
2024-12-31 03:45:51 +0300

    close the session, order by state id

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
             'mysql+mysqldb://{}:{}@localhost/{}'
             .format(sys.argv[1], sys.argv[2], sys.argv[3]),
             pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-states = session.query(State).all()
+states = session.query(State).order_by(State.id).all()
 for state in states:
     print("{}: {}".format(state.id, state.name))
+session.close()
