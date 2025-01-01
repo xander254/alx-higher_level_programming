050ae5d9d5780d4ee4db36188dc849d3e7ba182c
xander254 <alexkinyanjui356@gmail.com>
2025-01-02 02:33:47 +0300

    refactor

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,31 +1,47 @@
 #!/usr/bin/python3
-import sys
+"""
+Lists all cities of a state from the database hbtn_0e_4_usa.
+"""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=dbname)
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+        )
 
-    cur = db.cursor()
+        cur = db.cursor()
 
-    query = """SELECT cities.name
-               FROM cities
-               JOIN states ON cities.state_id = states.id
-               WHERE states.name = %s
-               ORDER BY cities.id ASC"""
+        query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE BINARY states.name = %s
+        ORDER BY cities.id ASC;
+        """
 
-    cur.execute(query, (state_name,))
+        cur.execute(query, (state_name,))
 
-    cities = cur.fetchall()
-    if cities:
-        print(", ".join([city[0] for city in cities]))
-    else:
-        print()
+        rows = cur.fetchall()
 
-    cur.close()
-    db.close()
+        print(", ".join([row[0] for row in rows]))
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+    finally:
+        if 'cur' in locals():
+            cur.close()
+        if 'db' in locals():
+            db.close()
