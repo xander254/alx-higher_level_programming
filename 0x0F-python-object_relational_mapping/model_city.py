82983f35abe1e7f161b7e9a31a533df66a55df60
xander254 <alexkinyanjui356@gmail.com>
2025-01-02 03:13:10 +0300

    fix whitespace

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin//python3
 """
-a Python file similar to model_state.py named model_city.py 
+a Python file similar to model_state.py named model_city.py
 that contains the class definition of a City.
 """
 from model_state import Base
