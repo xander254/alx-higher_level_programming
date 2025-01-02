0159481fd8e59958fe2319f7ae6f0840fd3c4241
xander254 <alexkinyanjui356@gmail.com>
2025-01-02 03:37:13 +0300

    debuggging

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
 """
-Prints all City objects from the database hbtn_0e_4_usa.
+Fetches and prints all City objects from the database hbtn_0e_14_usa,
+sorted by cities.id in ascending order.
 """
+
 import sys
-from model_city import City
-from model_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
 
 if __name__ == "__main__":
-    try:
-        engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                sys.argv[1], sys.argv[2], sys.argv[3]
-            ),
-            pool_pre_ping=True
-        )
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-        results = (
-            session.query(City, State)
-            .join(State, City.state_id == State.id)
-            .order_by(City.id.asc())
-            .all()
-        )
+    engine = create_engine(
+        f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/"
+        f"{database_name}",
+        pool_pre_ping=True
+    )
 
-        for city, state in results:
-            print(f"{state.name}: ({city.id}) {city.name}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        session.close()
+    results = (
+        session.query(City, State)
+        .join(State, City.state_id == State.id)
+        .order_by(City.id)
+        .all()
+    )
 
-    except Exception as e:
-        print(f"Error: {e}")
+    for city, state in results:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    session.close()
