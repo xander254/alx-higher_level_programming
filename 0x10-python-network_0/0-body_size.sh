228a29f04c3b4faea693cfd9a3269bb2c1f33c0a
xander254 <alexkinyanjui356@gmail.com>
2025-01-21 23:40:03 +0300

    response size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 # Get no of bytes in response
-URL="$1"
-SIZE=$(curl -sI "$URL" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
-if [ -n "$SIZE" ]; then
-    echo "$(SIZE)"
-fi
+echo $(curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
