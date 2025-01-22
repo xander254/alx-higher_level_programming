d2d2920bb5cd02f24ec0d7f7e7e59504895c3044
xander254 <alexkinyanjui356@gmail.com>
2025-01-22 05:24:57 +0300

    Documenting the module

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,11 @@ A module to sends a request to the URL and displays the X-Request-Id
 
 Usage:
     python3 script_name.py <url>
+Arguments:
+    <url> (str): The URL to which the GET request will be sent.
+
+Returns:
+    None: The script prints the 'X-Request-ID' header to the console.
 """
 
 
