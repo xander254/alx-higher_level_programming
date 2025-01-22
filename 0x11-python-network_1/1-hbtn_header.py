93c951afb806ad04cce0ea4a7e22a20772710d22
xander254 <alexkinyanjui356@gmail.com>
2025-01-22 05:18:41 +0300

    a sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A module to sends a request to the URL and displays the X-Request-Id"""
+
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+    request_id = response.headers.get('X-Request-ID')
+
+print(f"{request_id}\n")
