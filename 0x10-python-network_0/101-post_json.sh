7a613d731a4c5d211e0a11802bc6b7d879de052b
xander254 <alexkinyanjui356@gmail.com>
2025-01-22 04:11:28 +0300

    add json validation(wc -l == 3)... a script to send a json file to the server

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # A bash script to send a json file to the server using url
-#jq empty "$2" >/dev/null 2>&1
-#if [ $? -eq 0 ]; then
-#    echo "Valid JSON"
-#else 
-#    echo "Not a valid JSON"
-#    exit 1
-#fi
-
-curl -X POST -d @"$2" -H "Content-Type: application/json" "$1" 
-#echo ""
+jq empty "$2" >/dev/null 2>&1 || { echo "Not a valid JSON"; exit 1; } curl -X POST -d @"$2" -H "Content-Type: application/json" "$1" echo ""
