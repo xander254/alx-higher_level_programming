6004b8e54bc50f866bf4de8d3a679bce22cfb92c
xander254 <alexkinyanjui356@gmail.com>
2025-01-23 01:56:55 +0300

    A script that fetches the value of the X-Request-ID from a URL response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 """
-A module to sends a request to the URL and displays the X-Request-Id
-
-Usage:
-    python3 script_name.py <url>
-Arguments:
-    <url> (str): The URL to which the GET request will be sent.
-
-Returns:
-    None: The script prints the 'X-Request-ID' header to the console.
+A script that fetches the value of the X-Request-ID from a URL response header
 """
 
 
@@ -18,12 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    opener = urllib.request.build_opener(urllib.request.HTTPRedirectHandler())
-    with opener.open(url) as response:
-        # Get the value of the X-Request-ID from the header
+    with urllib.request.urlopen(url) as response:
         request_id = response.headers.get('X-Request-ID')
 
-    if request_id is None:
-        print("X-Request-ID: Not found")
-    else:
-        print(f"{request_id}\n")
+    print(request_id)
