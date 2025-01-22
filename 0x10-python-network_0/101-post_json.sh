e96ab3cdd30446b6ed9632e85abe3041ac94a000
xander254 <alexkinyanjui356@gmail.com>
2025-01-22 04:08:11 +0300

    silence json validation... a script to send a json file to the server

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # A bash script to send a json file to the server using url
-jq empty "$2" >/dev/null 2>&1
-if [ $? -eq 0 ]; then
-    echo "Valid JSON"
-else 
-    echo "Not a valid JSON"
-    exit 1
-fi
+#jq empty "$2" >/dev/null 2>&1
+#if [ $? -eq 0 ]; then
+#    echo "Valid JSON"
+#else 
+#    echo "Not a valid JSON"
+#    exit 1
+#fi
 
 curl -X POST -d @"$2" -H "Content-Type: application/json" "$1" 
-echo ""
+#echo ""
