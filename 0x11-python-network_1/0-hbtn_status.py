494d10ae0d3a90d4267a16d1a35cb722718a4ca4
xander254 <alexkinyanjui356@gmail.com>
2025-01-23 01:32:59 +0300

    A module to fetch and parse simple data from a URL using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-""" A module to fetch and parse simple data from a url using urllib"""
-
+""" A module to fetch and parse simple data from a URL using urllib """
 
 import urllib.request
+
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
-    encoding = response.headers.get_content_charset()
 
 print(
-    f"Body response:\n"
-    f" \t - type: {type(html)}\n"
-    f" \t - content: {html}\n"
-    f" \t - utf8 content: {html.decode('utf-8')}"
+    "Body response:\n"
+    f"\t- type: {type(html)}\n"
+    f"\t- content: {html}\n"
+    f"\t- utf8 content: {html.decode('utf-8')}"
 )
