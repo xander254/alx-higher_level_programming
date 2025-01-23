5bd1fb89f75efdbc57bb9b6325cc8cc216a18333
xander254 <alexkinyanjui356@gmail.com>
2025-01-23 04:19:36 +0300

    a script to chech http error given by url

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,10 +12,11 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        html = html.decode('utf-8')
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            html = html.decode('utf-8')
+            print(html)
 
-except urllib.HTTPError as e:
-    print(f("Error code: {e.code}")
+    except urllib.HTTPError as e:
+        print(f"Error code: {e.code}")
