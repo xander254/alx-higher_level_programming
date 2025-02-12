e6f31ad482c50766d54dd7a7dfe33cccc711dc0a
xander254 <alexkinyanjui356@gmail.com>
2025-02-12 05:55:33 +0300

    a script that gets the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const request = require('request');
 
 const url = process.argv[2];
-const file_name = process.argv[3];
+const fileName = process.argv[3];
 
-if (!url || file_name === undefined) {
-  console.error('Usage: ./5-request_store.js <url> <file_name>');
+if (!url || fileName === undefined) {
+  console.error('Usage: ./5-request_store.js <url> <fileName>');
   process.exit(1);
 }
 
@@ -17,7 +17,7 @@ request(url, function (error, reponse, body) {
     process, exit(1);
   }
 
-  fs.writeFile(file_name, body, 'utf-8', (err) => {
+  fs.writeFile(fileName, body, 'utf-8', (err) => {
     if (err) {
       console.error(err);
       process.exit(1);
