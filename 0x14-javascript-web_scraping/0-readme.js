8d39ae069fb9ae10309417a38c732b9b3ebd61ae
xander254 <alexkinyanjui356@gmail.com>
2025-02-12 04:43:59 +0300

    A js script to read the contents of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require("fs")
+const filepath = process.argv[2];
+
+if (!filepath) {
+  console.error("Provide the path of the file")
+  process.exit(1);
+}
+
+fs.readFile(filepath, "utf-8", (err, data) => {
+  if (err) {
+  console.error('${err.message}');
+  process.exit(1);
+  } 
+  console.log(data);
+});
+
